84a9e07d2c4b84527433eaf6ae6549655da67ad2
BenoGustavo <gustavoleandrogorges@gmail.com>
2024-04-12 16:12:44 -0300

    resize favicon

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from utils.model_validator import validate_png
+from utils.images import resize_image
 
 
 class MenuLink(models.Model):
@@ -37,8 +39,30 @@ class SiteSetup(models.Model):
     show_footer = models.BooleanField(default=True)
 
     favicon = models.ImageField(
-        upload_to="assets/favicon/%Y/%m/", default="", blank=True, null=True
+        upload_to="assets/favicon/%Y/%m/",
+        default="",
+        blank=True,
+        null=True,
+        validators=[validate_png],
     )
+
+    def save(self, *args, **kwargs):
+        # Check if the favicon has changed
+
+        # Get the current favicon name
+        current_favicon_name = str(self.favicon.name)
+
+        super().save(*args, **kwargs)
+
+        favicon_changed = False
+
+        # If has a favicon and the current favicon name is different from the new favicon name
+        if self.favicon:
+            favicon_changed = current_favicon_name != str(self.favicon.name)
+
+        # If the favicon has changed, resize it
+        if favicon_changed:
+            resize_image(self.favicon, new_width=32)
 
     def __str__(self):
         return self.tittle
