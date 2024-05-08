78d5c36ca580b915e458b3eef99e5bc217396d2c
BenoGustavo <gustavoleandrogorges@gmail.com>
2024-05-08 18:20:27 -0300

    forgot a comma

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -5,9 +5,17 @@ from blog.models import Tag, Categories, Page
 # Register your models here.
 @admin.register(Tag)
 class tagAdmin(admin.ModelAdmin):
-    list_display = "id", "name", "slug"
-    list_display_links = "name"
-    search_fields = "id", "name", "slug"
+    list_display = (
+        "id",
+        "name",
+        "slug",
+    )
+    list_display_links = ("name",)
+    search_fields = (
+        "id",
+        "name",
+        "slug",
+    )
     list_per_page = 10
     ordering = ("-id",)
     list_filter = ("name",)
@@ -16,9 +24,17 @@ class tagAdmin(admin.ModelAdmin):
 
 @admin.register(Categories)
 class categoriesAdmin(admin.ModelAdmin):
-    list_display = "id", "name", "slug"
-    list_display_links = "name"
-    search_fields = "id", "name", "slug"
+    list_display = (
+        "id",
+        "name",
+        "slug",
+    )
+    list_display_links = ("name",)
+    search_fields = (
+        "id",
+        "name",
+        "slug",
+    )
     list_per_page = 10
     ordering = ("-id",)
     list_filter = ("name",)
@@ -27,12 +43,25 @@ class categoriesAdmin(admin.ModelAdmin):
 
 @admin.register(Page)
 class pageAdmin(admin.ModelAdmin):
-    list_display = "id", "title", "slug", "is_published"
-    list_display_links = "title"
-    search_fields = "id", "title", "slug", "is_published"
+    list_display = (
+        "id",
+        "title",
+        "slug",
+        "is_published",
+    )
+    list_display_links = ("title",)
+    search_fields = (
+        "id",
+        "title",
+        "slug",
+        "is_published",
+    )
     list_per_page = 50
     ordering = ("-id",)
     list_filter = ("title", "is_published")
     prepopulated_fields = {"slug": ("title",)}
-    readonly_fields = "created_at", "updated_at"
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
     list_editable = ("is_published",)
