a97ea4b8833316d7fc4f77caffc713eb7b74fb15
BenoGustavo <gustavoleandrogorges@gmail.com>
2024-05-14 22:14:16 -0300

    now the posts work

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -21,6 +21,54 @@ def index(request):
     )
 
 
+def created_by(request, author_id: int):
+    posts = Post.objects.get_published().filter(created_by__pk=author_id)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        "blog/pages/index.html",
+        {
+            "page_obj": page_obj,
+        },
+    )
+
+
+def category(request, slug):
+    posts = Post.objects.get_published().filter(categories__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        "blog/pages/index.html",
+        {
+            "page_obj": page_obj,
+        },
+    )
+
+
+def tag(request, slug):
+    posts = Post.objects.get_published().filter(tags__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        "blog/pages/index.html",
+        {
+            "page_obj": page_obj,
+        },
+    )
+
+
 def page(request):
     return render(
         request,
@@ -31,11 +79,13 @@ def page(request):
     )
 
 
-def post(request):
+def post(request, slug):
+    post = Post.objects.get_published().filter(slug=slug).first()
+
     return render(
         request,
         "blog/pages/post.html",
         {
-            # "page_obj": page_obj,
+            "post": post,
         },
     )
